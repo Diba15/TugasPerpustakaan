9cc253f03fc3aa8bb59f6f1e5f1e149c1ea10226
Diba15 <44836047+Diba15@users.noreply.github.com>
2020-11-14 19:49:49 +0700

    Commit Default

--- src/Majalah.java
+++ src/Majalah.java
@@ -20,8 +20,8 @@ public class Majalah extends Koleksi {
     @Override
     public void display() {
         System.out.println("No Koleksi : "+getNoKoleksi());
-        System.out.println("Nama  : "+getNama()+"\tPenerbit : "+getPenerbit());
-        System.out.println("Jenis : "+getJenis()+"\nStatus : "+getStatus());
+        System.out.println("Nama  \t: "+getNama()+"\t\tPenerbit : "+getPenerbit());
+        System.out.println("Jenis \t: "+getJenis()+"\nStatus \t: "+getStatus());
         System.out.println("Tanggal Terbit : "+getTanggalTerbit());
         System.out.println("=======================================================");
         System.out.println("Info Majalah :");
